25358f43619bac1fd35ea031665f0537a375fc7a
Sharad Sharma <119737825+sharmasharad311@users.noreply.github.com>
2024-03-01 09:42:33 +0530

    Update script.js

--- promiseJS/script.js
+++ promiseJS/script.js
@@ -1,6 +1,7 @@
-console.log("hello JavaScript");
-// so we are going to learn What are Promises in JavaScript.
 
+----------------------Method First
+
+// so we are going to learn What are Promises in JavaScript.
 const promisesOne = new Promise(function(resolve, reject){
     setTimeout(()=>{
         console.log("promise is working.")
@@ -11,4 +12,15 @@ const promisesOne = new Promise(function(resolve, reject){
 //.then work when we pass resolve() in line 7th
 promisesOne.then(function(){
     console.log("promise value inside the then method")
+});
+
+-------------------------Method Second
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("promise 2 is working.")
+    }, 2000)
+    resolve();
+}).then(function(){
+    console.log("yes 2nd resolve is active")
 });

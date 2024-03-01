6c8a5c554615a8d8873780081b379f5538d8eafa
Sharad Sharma <119737825+sharmasharad311@users.noreply.github.com>
2024-03-01 05:05:53 +0000

    "committed"

--- promiseJS/script.js
+++ promiseJS/script.js
@@ -1,26 +1,67 @@
 
-----------------------Method First
+// ----------------------Method First
 
 // so we are going to learn What are Promises in JavaScript.
 const promisesOne = new Promise(function(resolve, reject){
     setTimeout(()=>{
-        console.log("promise is working.")
+        // console.log("promise is working.")
         resolve();
     }, 2000)
 })
 
 //.then work when we pass resolve() in line 7th
 promisesOne.then(function(){
-    console.log("promise value inside the then method")
+    // console.log("promise value inside the then method")
 });
 
--------------------------Method Second
+// -------------------------Method Second
 
 new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("promise 2 is working.")
+        // console.log("promise 2 is working.")
     }, 2000)
     resolve();
 }).then(function(){
-    console.log("yes 2nd resolve is active")
+    // console.log("yes 2nd resolve is active")
 });
+
+
+// ------------------------Method Three
+new Promise(function(pass, fail){
+    setTimeout(function(){
+        // console.log("inside the timeout Fn")
+        pass({user: "sharad sharma", id: 123})
+    }, 1000)
+}).then(function(userValue){
+    // console.log(userValue.user);
+})
+
+// -------------------------Method Four
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(()=>{
+        let value = true;
+        if(!value){
+            resolve({user: "Sharad", lastname:"Sharma"})
+        }else{
+            reject(()=>{
+                console.log("ERROR")
+            })
+        }
+    }, 2000)
+});
+promiseFour
+.then(function(valueone){
+    console.log(valueone);
+    return valueone.user;
+}).then(function(user){
+    console.log(user);
+    return user;
+}).then(function(valueone){
+    console.log(valueone.lastname);
+}).catch(function(){
+    console.log("yes it's error: 400");
+}).finally(()=>{
+   setTimeout(() => {
+    console.log("Hope you understand the Pomises and promise chaining")
+   }, 2000);
+})
